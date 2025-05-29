953e90b2962af7687b64a3ca70f78320fffdaf0e
ArikusumaWardana <arikusumawardana09@gmail.com>
2025-05-29 16:22:52 +0800

    Add getCategories function: Implement a new function to fetch categories from the database, handling errors and returning an empty array if an error occurs.

---
+++ src/app/(admin)/dashboard/categories/lib/data.ts
@@ -0,0 +1,15 @@
+import prisma  from "../../../../../../lib/prisma";
+
+export async function getCategories() {
+     try {
+          const categories = await prisma.categories.findMany({
+               where: {
+                    deleted_at: null
+               }
+          })
+          return categories;
+     } catch (error) {
+          console.error("Error fetching categories:", error);
+          return [];
+     }
+}
