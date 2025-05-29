124276e17ae12b43a82e78aa11b71ad6a4b24d1f
ArikusumaWardana <arikusumawardana09@gmail.com>
2025-05-29 18:13:19 +0800

    Add category schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -4,3 +4,8 @@ export const loginSchema = z.object({
      email: z.string({required_error: 'Email is required'}).email({message: 'Invalid email address'}),
      password: z.string({required_error: 'Password is required'}).min(8, {message: 'Password must be at least 8 characters long'}),
 })
+
+export const categorySchema = z.object({
+     code: z.string({required_error: 'Code is required'}).min(4, {message: 'Code must be at least 4 characters long'}),
+     name: z.string({required_error: 'Name is required'}).min(4, {message: 'Name must be at least 4 characters long'}),
+})
