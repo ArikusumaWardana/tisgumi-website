98bdf87551bc7c872fb7d383b6b476ff46fb3fb1
ArikusumaWardana <arikusumawardana09@gmail.com>
2025-05-30 12:14:06 +0800

    Add adminSchema to schema.ts

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -17,3 +17,11 @@ export const customerSchema = z.object({
      status: z.enum(['active', 'inactive'], { required_error: 'Status is required' }),
 })
 
+export const adminSchema = z.object({
+     code: z.string({required_error: 'Code is required'}).min(4, {message: 'Code must be at least 4 characters long'}),
+     name: z.string({ required_error: 'Name is required' }).min(4, { message: 'Name must be at least 4 characters long' }),
+     email: z.string({ required_error: 'Email is required' }).email({ message: 'Invalid email address' }),
+     phone: z.string({ required_error: 'Phone is required' }).min(10, { message: 'Phone must be at least 10 characters long' }),
+     password: z.string({ required_error: 'Password is required' }).min(8, { message: 'Password must be at least 8 characters long' }),     
+})
+
