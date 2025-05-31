0884c1e6c67b1f2564b4b1c0df813e368ae0ba0e
ArikusumaWardana <arikusumawardana09@gmail.com>
2025-05-31 15:00:46 +0800

    Refactor Prisma schema to update Order and Invoice models, and replace InvoiceItem with OrderItem. Adjust relationships and add necessary indexes

--- prisma/migrations/20250531065704_init/migration.sql
+++ prisma/migrations/20250531065704_init/migration.sql
@@ -5,7 +5,7 @@ CREATE TYPE "Role" AS ENUM ('admin', 'superadmin');
 CREATE TYPE "Status" AS ENUM ('active', 'inactive');
 
 -- CreateEnum
-CREATE TYPE "InvoiceStatus" AS ENUM ('lunas', 'belum_lunas');
+CREATE TYPE "OrderPaymentStatus" AS ENUM ('lunas', 'belum_lunas');
 
 -- CreateTable
 CREATE TABLE "users" (
@@ -26,7 +26,7 @@ CREATE TABLE "users" (
 -- CreateTable
 CREATE TABLE "sessions" (
     "id" TEXT NOT NULL,
-    "user_id" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
     "expiresAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
@@ -37,7 +37,6 @@ CREATE TABLE "categories" (
     "id" SERIAL NOT NULL,
     "code" VARCHAR(50) NOT NULL,
     "name" VARCHAR(100) NOT NULL,
-    "status" "Status" NOT NULL DEFAULT 'active',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
@@ -50,8 +49,7 @@ CREATE TABLE "products" (
     "id" SERIAL NOT NULL,
     "code" VARCHAR(50) NOT NULL,
     "name" VARCHAR(100) NOT NULL,
-    "description" TEXT,
-    "default_price" INTEGER NOT NULL,
+    "default_price" INTEGER NOT NULL DEFAULT 0,
     "status" "Status" NOT NULL DEFAULT 'active',
     "category_id" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -76,27 +74,36 @@ CREATE TABLE "customers" (
 );
 
 -- CreateTable
-CREATE TABLE "invoices" (
+CREATE TABLE "orders" (
     "id" SERIAL NOT NULL,
     "code" VARCHAR(50) NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "customer_id" INTEGER NOT NULL,
     "created_by_user_id" INTEGER NOT NULL,
-    "show_price" BOOLEAN NOT NULL DEFAULT true,
-    "status" "InvoiceStatus" NOT NULL DEFAULT 'lunas',
-    "file_location" TEXT[],
+    "status" "OrderPaymentStatus" NOT NULL DEFAULT 'lunas',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "invoices" (
+    "id" SERIAL NOT NULL,
+    "order_id" INTEGER NOT NULL,
+    "file_url" VARCHAR(255) NOT NULL,
+    "show_price" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "invoices_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "invoice_items" (
+CREATE TABLE "order_items" (
     "id" SERIAL NOT NULL,
     "code" VARCHAR(50) NOT NULL,
-    "invoice_id" INTEGER NOT NULL,
+    "order_id" INTEGER NOT NULL,
     "product_id" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
     "price_at_time" INTEGER NOT NULL,
@@ -104,7 +111,7 @@ CREATE TABLE "invoice_items" (
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
 
-    CONSTRAINT "invoice_items_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "order_items_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -128,7 +135,7 @@ CREATE UNIQUE INDEX "users_code_key" ON "users"("code");
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "sessions_id_key" ON "sessions"("id");
+CREATE INDEX "sessions_userId_idx" ON "sessions"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "categories_code_key" ON "categories"("code");
@@ -137,34 +144,61 @@ CREATE UNIQUE INDEX "categories_code_key" ON "categories"("code");
 CREATE UNIQUE INDEX "products_code_key" ON "products"("code");
 
 -- CreateIndex
+CREATE INDEX "products_category_id_idx" ON "products"("category_id");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "customers_code_key" ON "customers"("code");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "invoices_code_key" ON "invoices"("code");
+CREATE UNIQUE INDEX "orders_code_key" ON "orders"("code");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "invoice_items_code_key" ON "invoice_items"("code");
+CREATE INDEX "orders_customer_id_idx" ON "orders"("customer_id");
+
+-- CreateIndex
+CREATE INDEX "orders_created_by_user_id_idx" ON "orders"("created_by_user_id");
+
+-- CreateIndex
+CREATE INDEX "invoices_order_id_idx" ON "invoices"("order_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "order_items_code_key" ON "order_items"("code");
+
+-- CreateIndex
+CREATE INDEX "order_items_order_id_idx" ON "order_items"("order_id");
+
+-- CreateIndex
+CREATE INDEX "order_items_product_id_idx" ON "order_items"("product_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "custom_product_pricings_code_key" ON "custom_product_pricings"("code");
 
+-- CreateIndex
+CREATE INDEX "custom_product_pricings_customer_id_idx" ON "custom_product_pricings"("customer_id");
+
+-- CreateIndex
+CREATE INDEX "custom_product_pricings_product_id_idx" ON "custom_product_pricings"("product_id");
+
 -- AddForeignKey
-ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "invoices" ADD CONSTRAINT "invoices_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "orders" ADD CONSTRAINT "orders_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "invoices" ADD CONSTRAINT "invoices_created_by_user_id_fkey" FOREIGN KEY ("created_by_user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "orders" ADD CONSTRAINT "orders_created_by_user_id_fkey" FOREIGN KEY ("created_by_user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "invoice_items" ADD CONSTRAINT "invoice_items_invoice_id_fkey" FOREIGN KEY ("invoice_id") REFERENCES "invoices"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "invoices" ADD CONSTRAINT "invoices_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "invoice_items" ADD CONSTRAINT "invoice_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "order_items" ADD CONSTRAINT "order_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "custom_product_pricings" ADD CONSTRAINT "custom_product_pricings_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
