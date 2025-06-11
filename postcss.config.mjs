8e90176b99d2915aca1512f6bceba25a0ad27564
ArikusumaWardana <arikusumawardana09@gmail.com>
2025-06-11 21:06:05 +0800

    Add autoprefixer and related dependencies to package.json and package-lock.json. Update postcss.config.mjs to use new plugin structure for Tailwind CSS.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,33 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [
-    "@tailwindcss/postcss",
-    ...(process.env.NODE_ENV === "production"
-      ? [
-          [
-            "cssnano",
-            {
-              preset: [
-                "advanced",
-                {
-                  discardComments: {
-                    removeAll: true,
-                  },
-                  normalizeWhitespace: true,
-                  colormin: true,
-                  convertValues: true,
-                  discardDuplicates: true,
-                  discardEmpty: true,
-                  mergeRules: true,
-                  minifyFontValues: true,
-                  minifyParams: true,
-                  minifySelectors: true,
-                },
-              ],
-            },
-          ],
-        ]
-      : []),
-  ],
+  plugins: {
+    tailwindcss: {},
+  },
 };
 
 export default config;
